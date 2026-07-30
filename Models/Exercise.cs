4b5492f38e96912c5f85fbc4d5106fcca0e20ac9
John Henry <jland5th@gmail.com>
2026-07-30 13:38:09 -0500

    Add basic menu

---
+++ Models/Exercise.cs
@@ -0,0 +1,8 @@
+namespace GymTracker.Models;
+
+public class Exercise{
+    public string Name {get; set;} = "";
+    public int Sets {get; set;} = 0;
+    public int Reps {get; set;} = 0;
+    public int Weight {get; set;} = 0;
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("=== Gym Tracker ===");
+    Console.WriteLine("1. Add Workout");
+    Console.WriteLine("2. View Workouts");
+    Console.WriteLine("0. Exit");
+
+    var choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            Console.WriteLine("Add Workout");
+            Console.ReadKey();
+            break;
+
+        case "2":
+            Console.WriteLine("View Workouts");
+            Console.ReadKey();
+            break;
+
+        case "0":
+            return;
+    }
+}
